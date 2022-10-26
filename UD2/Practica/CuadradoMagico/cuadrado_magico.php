894220e5822323fc20c5d5919fb035ecdf3912ff
margaMF <mmoyafiol@gmail.com>
2022-10-26 16:35:08 +0200

    Estructura carpetas y parte grafica

---
+++ UD2/Practica/CuadradoMagico/cuadrado_magico.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cuadrado Mágico</title>
+    <link rel="stylesheet" href="css/estilos_cuadrado_magico.css">
+</head>
+<body>
+    <h1>CUADRADO MÁGICO</h1>
+    <table>
+        <tr>
+            <td>4</td>
+            <td>9</td>
+            <td>2</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>5</td>
+            <td>7</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>1</td>
+            <td>6</td>
+        </tr>
+
+    </table>
+</body>
+</html>
